0975747a205d41cd3867ca1778695c41b57ba1fc
FrownyFace <soham.waychal@gmail.com>
2015-04-11 00:25:50 -0500

    Backend Done
    Yolo

--- models/tasks.js
+++ models/tasks.js
@@ -1,12 +1,14 @@
 var mongoose = require('mongoose');
 var tasksScheme   = new mongoose.Schema({
+  _id: {type: mongoose.Schema.ObjectId, auto: true, select: true},
+  __v: {type: Number, select: false},
   name: String,
   description: String,
-  pendingTasks - String,
-  deadline - Date,
-  completed - Boolean,
-  assignedUser - String,
-  assignedUserName - String,
-  dateCreated - Date
+  pendingTasks: String,
+  deadline: Date,
+  completed: Boolean,
+  assignedUser: String,
+  assignedUserName: String,
+  dateCreated: Date
 });
-module.exports = mongoose.model('Users', tasksScheme);
+module.exports = mongoose.model('Tasks', tasksScheme);
--- models/users.js
+++ models/users.js
@@ -1,8 +1,10 @@
 var mongoose = require('mongoose');
 var usersScheme   = new mongoose.Schema({
+  _id: {type: mongoose.Schema.ObjectId, auto: true, select: true},
+  __v: {type: Number, select: false},
   name: String,
   email: String,
-  pendingTasks - String,
-  dateCreated - Date
+  pendingTasks: [String],
+  dateCreated: {type:Date,default:Date.now}
 });
 module.exports = mongoose.model('Users', usersScheme);
