be29d6a55aa29154e18a51d711308e8b1396a54d
FrownyFace <soham.waychal@gmail.com>
2015-04-12 22:16:11 -0500

    Final Final
    ._.

--- server.js
+++ server.js
@@ -213,7 +213,7 @@ router.put('/users/:id',function(req,res,next){
       return
     }
     var id = '{"message": "User Updated","data":'+JSON.stringify(put)+'}';
-    res.statusCode = 200;
+    res.statusCode = 201;
     id = JSON.parse(id);
     res.json(id);
   });
@@ -285,7 +285,7 @@ router.post('/tasks',function(req,res,next){
       return
     }
     var id = '{"message": "Task Added","data":'+JSON.stringify(post)+'}';
-    res.statusCode = 200;
+    res.statusCode = 201;
     id = JSON.parse(id);
     res.json(id);
     });
